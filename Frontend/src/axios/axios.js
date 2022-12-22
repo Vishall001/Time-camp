fc17374ccbee75ef864dc3d48057013c0c8c3ffa
Vishall001 <vshmandloi@gmail.com>
2022-12-22 20:54:01 +0530

    backend deployed link updated

--- Frontend/src/axios/axios.js
+++ Frontend/src/axios/axios.js
@@ -1,7 +1,6 @@
-
-import axios from "axios"
+import axios from "axios";
 
 export default axios.create({
-    // baseURL:"http://localhost:4000"
-    baseURL:"https://timescamp.herokuapp.com/"
-})
+  // baseURL:"http://localhost:4000"
+  baseURL: "https://backend-timecamp.onrender.com/",
+});
